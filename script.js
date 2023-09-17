4c089dd4af33386d2a1a6ef941830139651a056a
sykul <luke.flatley@outlook.com>
2023-09-17 16:47:49 +0200

    make square divs 50px x 50px

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ document.body.onload = addGrid;
 function addGrid() {
     const containerDiv = document.querySelector(".grid");
     for (i=0; i<256; i++) {
-        const square = document.createElement("div");
+        let square = document.createElement("div");
+        square.className = "square";
         containerDiv.appendChild(square);
+        square.style.height = '50px';
+        square.style.width = '50px';
     }
+
 }
+
