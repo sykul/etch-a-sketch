60c836810a270d2e8964e3b8cbccd3ccdcb7df01
sykul <luke.flatley@outlook.com>
2023-09-18 23:27:53 +0200

    save changes

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-document.body.onload = addGrid;
 
-function addGrid() {
+let gridSize = 16;
+document.body.onload = addGrid(gridSize);
+
+console.log(gridSize);
+
+function addGrid(gridSize) {
     const containerDiv = document.querySelector(".grid");
-    for (i=0; i<256; i++) {
+    containerDiv.innerHTML = "";
+
+    if (gridSize > 100) {
+        gridSize = 100;
+    } else if (gridSize == 0 || isNaN(gridSize)) {
+        gridSize = 1;
+    }
+
+    for (i=0; i<(gridSize*gridSize); i++) {
         let square = document.createElement("div");
         square.className = "square";
         containerDiv.appendChild(square);
-        square.style.height = '40px';
-        square.style.width = '40px';
+        square.style.height = `calc(100%/${gridSize})`;
+        square.style.flex = `1 1 calc(100%/${gridSize})`
         square.addEventListener('mouseover', () => {
             square.classList.add('blackSquare');
         })
@@ -15,8 +27,9 @@ function addGrid() {
 }
 
 function chooseSize() {
-    const gridSize = prompt("Choose your size.");
-    return gridSize;
+    const gridSize = prompt("Choose your size, N*N, up to 100.");
+    addGrid(gridSize);
 }
+
 
 document.querySelector("#sizeButton").addEventListener('click', chooseSize);
